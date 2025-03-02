6721d5aa50e36c3176aff6a0fbdd8ad2b82b6335
Nipen Khant <khantnipen@gmail.com>
2025-03-02 14:39:01 +0530

    database setup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
  apply this commmand ==? source  env/Scripts/activate
 
- pyhton manage.py runserver 0.0.0.0:8000
+ python manage.py runserver 0.0.0.0:8000
 
 
  http://192.168.1.12:8000
--- backend/settings.py
+++ backend/settings.py
@@ -90,25 +90,22 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("DB_USER"),
-#         'PASSWORD': os.getenv("DB_PASSWORD"),
-#         'HOST': os.getenv("DB_HOST"),        
-#         'PORT': os.getenv("DB_PORT"),
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # Make sure this is correct
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),  # AWS RDS or local DB
+        'PORT': os.getenv("DB_PORT", '5432'),  # Default PostgreSQL port
+    }
+}
 
 
 # Password validation
